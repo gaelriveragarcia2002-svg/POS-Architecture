8ae5c49608a3a8e4688d4a6741a03965ba7a595e
Gael Rivera Garcia <gael.rivera@legux.com>
2026-09-17 14:48:38 -0600

    refactor: :recycle: Migracion a Playwright en lugar de Cypress.

--- apps/POS-Architecture-e2e/eslint.config.mjs
+++ apps/POS-Architecture-e2e/eslint.config.mjs
@@ -1,11 +1,12 @@
-import cypress from "eslint-plugin-cypress/flat";
-import baseConfig from "../../eslint.config.mjs";
+import playwright from 'eslint-plugin-playwright';
+import baseConfig from '../../eslint.config.mjs';
 
 export default [
-    cypress.configs["recommended"],
-    ...baseConfig,
-    {
-        // Override or add rules here
-        rules: {}
-    }
+  playwright.configs['flat/recommended'],
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    // Override or add rules here
+    rules: {},
+  },
 ];
