959c9d1e1e644a06875973c8fa7a1f75ac51f7d5
Gael Rivera Garcia <gael.rivera@legux.com>
2026-09-18 13:23:40 -0600

    feat: :sparkles: Implementacion de ruta inicial de db para poder interactuar con la base local con una ruta.

--- apps/Check-App/src/app/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-export const items = sqliteTable('items', {
-  id: text('id').primaryKey(),
-  name: text('name').notNull(),
-  quantity: integer('quantity').default(0),
-  createdAt: text('created_at').notNull(),
-});
